bd9969683be963b0cfc133c5711d2f857feb44a2
Metaray <metaray21@gmail.com>
2019-03-03 09:26:49 +0300

    Now targeting 4.5.1 framework

--- MouseTracer/MouseHook.cs
+++ MouseTracer/MouseHook.cs
@@ -123,12 +123,13 @@ namespace MouseTracer
 
         private enum MouseMessages
         {
+            WM_MOUSEMOVE = 0x0200,
             WM_LBUTTONDOWN = 0x0201,
             WM_LBUTTONUP = 0x0202,
-            WM_MOUSEMOVE = 0x0200,
-            WM_MOUSEWHEEL = 0x020A,
             WM_RBUTTONDOWN = 0x0204,
-            WM_RBUTTONUP = 0x0205
+            WM_RBUTTONUP = 0x0205,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_MOUSEHWHEEL = 0x020E,
         }
 
         [StructLayout(LayoutKind.Sequential)]
